ccb93c842efbae501354e028ab0d952a02148b64
Viktoria <wikusechka@gmail.com>
2017-04-06 17:44:52 +0300

    LabWork
    LabWork №2

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Git_Hub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2.1. Базовi елементи мови С#. Варiант 9");
+
+            Console.WriteLine("\nЗавдання 1. Введiть цiлi данi: ");
+            Console.Write("Сумма вклада - ");
+            int summa = int.Parse(Console.ReadLine());
+            Console.Write("Процент по вкладу - ");
+            int percent = int.Parse(Console.ReadLine());
+            int sumagod = summa + (summa * percent) / 100;
+            Console.WriteLine("Сума - " + sumagod + " грн.");
+            Console.ReadKey();
+        }
+
+    }
+}
